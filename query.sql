49f84ae64478efa2b1cc0147126ad4a6c59b70b0
Alif-Sarker <sarker202454@gmil.com>
2025-12-22 17:11:46 +0600

    query solition added in table

---
+++ query.sql
@@ -0,0 +1,20 @@
+--Query -1 
+select booking_id,name,vehicle_name,start_date,end_date,booking_status from bookings
+join users on bookings.user_id = users.user_id 
+join vehicles on bookings.vehicle_id = vehicles.vehicle_id;
+
+
+--Query - 2
+select * from vehicles 
+where status = 'available' or status = 'maintenance'
+
+
+--Query-3
+select * from vehicles 
+where type = 'car'
+
+
+--Query 4 
+select vehicle_name , count(*) as total_bookings  from  bookings
+join vehicles on bookings.vehicle_id = vehicles.vehicle_id 
+group by vehicle_name having count(*) > 2;
